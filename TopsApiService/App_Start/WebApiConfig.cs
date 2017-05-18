a03fa8768b17d2e3703d2bb66eda44f5a2a3dd68
sjanchom <sjanchom@gmail.com>
2017-05-18 16:09:17 +0700

    add cors

--- TopsApiService/App_Start/WebApiConfig.cs
+++ TopsApiService/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace TopsApiService
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
